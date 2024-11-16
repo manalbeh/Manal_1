25c528e777540f4a6c83f800e666c2c35517f79b
manalbeh <manel6494@gmail.com>
2024-11-16 03:29:22 +0100

    Routing and url generation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #routage par attribut
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return new Response(content: "Hello World !");
+    }
+    #[Route('/about/{entier}', name: 'app_about', requirements: ['entier' => '\d+'])]
+    public function about(string $entier = "776"): Response
+    {
+        return new Response(content: "Hello ".$entier);
+    }
+    #[Route('/about/contact', name: 'contact_about')]
+    public function contact(): Response
+    {
+        return new Response(content: "Bienvenue");
+    }
+}
